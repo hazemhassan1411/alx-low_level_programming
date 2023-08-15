775534c34f761281e053209905ab49aad1b1f67a
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-15 02:05:32 -0700

    d

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*
+ * main - print putchar
+ * Description: print putchar with new line
+ * Return: always 0
+ */
+
+
+
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
