fdc98b955ed6270feceb5eef95014697f68f5f77
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-16 05:40:16 -0700

    f

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - write alphabet
+ * Descripthion:  prints all the numbers of base 16 in lowercase
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int number = 48;
+
+	while (number <= 102)
+	{
+	putchar(number);
+
+	if (number == 57)
+		number += 39;
+
+	}
+
+	putchar('\n');
+return (0);
+}
