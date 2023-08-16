c2970ecd418d364f338c2d5da25f6c2041812710
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-16 05:59:08 -0700

    f

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - write alphabet
+ * Descripthion:  prints all possible combinations of single-digit numbers.
+ * Return: always 0
+*/
+
+int main(void)
+{
+
+	int number = 0;
+
+	while (number <= 9)
+	{
+	putchar(number + 48);
+
+	if (number != 9)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+	number++;
+
+	putchar('\n');
+		return (0);
+}
