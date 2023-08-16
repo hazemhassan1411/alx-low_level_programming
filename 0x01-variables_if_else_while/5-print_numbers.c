8cfe4acc824a11d681cca21ae6bd17d5fde85fa6
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-16 05:01:44 -0700

    f

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - write alphabet
+ * Descripthion: prints the numbers
+ * Return: always 0
+*/
+
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+{
+	printf("%i", number);
+		number++;
+}
+putchar("\n");
+return (0);
+}
