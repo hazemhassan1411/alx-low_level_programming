814e364a7b25c4aad1e0ba5dcc66893338771919
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-18 14:32:24 -0700

    j

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - utillize on the _putchar functhin to print alpabet 10 times
+ * print_alphabet_x10 - utillize on the _putchar functhin to print alpabet 10 
  */
 
 void print_alphabet_x10(void)
