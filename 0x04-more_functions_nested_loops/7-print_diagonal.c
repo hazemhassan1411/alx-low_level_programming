34d483dc62fd929428aa7eab5a3949f9de2de261
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-18 16:41:17 -0700

    l

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,26 @@
+#include  "main.h"
+
+/**
+ * print_diagonal - print a digagonal
+ *
+ * @n: is anumber
+ *
+ */
+
+void print_diagonal(int n)
+{
+	int j, f;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (j = 1; j <= n; j++)
+		{
+			for (f = 1; f <= j; f++)
+				_putchar(' ');
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
