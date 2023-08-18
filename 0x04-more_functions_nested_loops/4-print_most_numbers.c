71f81e13094f8665e6c73408417acb2dff5cd0bd
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-18 15:50:34 -0700

    k

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -13,7 +13,7 @@ void print_most_numbers(void)
 	for (number >= 0; number <= 9; number++)
 	{
 
-		if (number == 4 || number == 2)
+		if (number == 2 || number == 4)
 			continue;
 		_putchar(number + 48);
 	}
