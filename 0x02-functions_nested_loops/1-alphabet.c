f9e0af881b7073d41f8847a497b4787a505ead68
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-18 14:16:54 -0700

    k

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,16 +4,12 @@
  * to a-z
  */
 
-
-
 void print_alphabet(void)
 {
 
-int ch;
+	int ch;
 
-for (ch = 'a'; ch <= 'z'; ch++)
-
-			_putchar(ch);
-		_putchar('\n');
-
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
 }
