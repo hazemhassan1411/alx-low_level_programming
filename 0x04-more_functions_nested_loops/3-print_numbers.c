8323a5b099e745f21825758abbfe241c33ad3474
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-18 15:31:14 -0700

    k

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - is function
+ * Return: 0
+ *
+ */
+
+
+
+void print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n');
+}
