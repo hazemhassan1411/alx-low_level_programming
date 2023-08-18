a2a285e915ae9c0e3ce4a4864cd340343bf9ef43
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-18 06:41:06 -0700

    h

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,12 +8,12 @@
 
 
 
-int main (void)
+int main(void)
 {
 	char stringe[] = "_putchar";
-	int chara
+	int chara;
 
-	for(chara = 0; chara < 8; chara++)
+	for (chara = 0; chara < 8; chara++)
 		_putchar(stringe[chara]);
 	_putchar("\n");
 
