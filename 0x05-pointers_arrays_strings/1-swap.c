2f9fd5de9fc2881b0371c6f12bd6de683176e5ff
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-08-21 04:51:52 -0700

    j

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * swap_int - swap to integrs
+ * @a: an integr
+ * @b: an integr
+ *
+ */
+
+
+
+void swap_int(int *a, int *b)
+{
+
+	int kk;
+
+	kk = *a;
+	*a = *b;
+	*b = kk;
+
+
+}
