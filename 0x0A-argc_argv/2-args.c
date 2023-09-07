905deb5ada0c9632646435115859c4f7644d57dc
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-07 00:41:27 -0700

    k

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - it is the main
+ * @argc: it is avar
+ * @argv: it is a var
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	while (argc--)
+	{
+
+		printf("%s\n", argv[i]);
+		i++;
+	}
+}
