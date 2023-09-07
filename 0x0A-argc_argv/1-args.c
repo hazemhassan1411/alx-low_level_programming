f941a283bed73c9e35964a4cdc8dfbd3e7c9684c
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-07 00:25:00 -0700

    k

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - it is the main
+ * @argc: it is avar
+ * @argv: it is a var
+ * Return: return 0
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
