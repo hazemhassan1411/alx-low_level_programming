295789c55b20644a516db50d2a0fabfd184a8e70
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-11 18:01:09 -0700

    j

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef _DOG_H
+#define _DOG_H
+/**
+ * struct dog - it is struct
+ * @name: it is name
+ * @owner: it is owner
+ * @age: it is age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
