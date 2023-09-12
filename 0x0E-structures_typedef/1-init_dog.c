274b007f8324aac72f52db059691c8cc9d50e668
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-11 18:05:03 -0700

    j

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *  * struct dog - it is struct
+ *   * @name: it is name
+ *    * @owner: it is owner
+ *     * @age: it is age
+ *      */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
+#endif
