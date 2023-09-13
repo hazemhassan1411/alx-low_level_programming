035d13e66692cf6ac36ad5d3784387f7904516ae
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-13 02:12:17 -0700

    gf

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - it is a function
+ * @str: it is astring
+ * Return: return m
+ */
+
+char *_strdup(char *str)
+{
+	int h, size = 0;
+	char *m;
+
+	for (; str[size] != '\0'; size++)
+
+	if (str == 0)
+		return (NULL);
+
+	m = malloc(size * sizeof(*str) + 1);
+
+	if (m == 0)
+	{
+		return (NULL);
+	}
+	else
+	{
+		for (; h < size; h++)
+			m[h] = str[h];
+	return (0);
+	}
+	return (m);
+}
