3b925553e278a082609922b96495e462820b4011
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-14 09:23:51 -0700

    j

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,5 +12,5 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 	if (array = NULL || action = NULL)
 		return;
 	for (size_t i = 0; i < size; i++)
-		action(*array);
+		action(array[i]);
 }
