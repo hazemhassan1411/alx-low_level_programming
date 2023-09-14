4d406356789af4217053c2d37356d648c406114a
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-14 09:11:34 -0700

    j

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+/**
+ * array_iterator - it is a function
+ * @array: it is an array
+ * @size: it is the size of array
+ * @action: it is call function
+ * Return: return 0
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+		action(*array);
+}
