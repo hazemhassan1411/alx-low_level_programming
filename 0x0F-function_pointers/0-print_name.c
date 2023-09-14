a6214b0380f9ded195d291d76d0211666f7c405d
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-14 08:32:51 -0700

    g

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+/**
+ * print_name - it is a function
+ * @name: it is a name
+ * @f: it is pointer to func
+ * Return: name
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name || f)
+		f(name);
+}
