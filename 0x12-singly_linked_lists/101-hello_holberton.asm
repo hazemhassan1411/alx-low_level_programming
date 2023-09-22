28b1d9361304f7476acfc26d500d4ea77f8820b1
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-21 19:43:20 -0700

    f

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,10 @@
-section .data
-    hello db "Hello, Holberton",10  ; The string to be printed, followed by a newline
-    hello_len equ $ - hello         ; Calculate the length of the string
-
-section .text
-    global main
-    extern printf
-
+	global   main
+	  exetrn    printf
 main:
-    ; Call printf to print the string
-    mov rdi, hello               ; Set the format string address (1st argument)
-    call printf                 ; Call printf function
+	mov	edi, format
+	xor	eax, eax
+	call	printf
+	mov	eax, 0
+	ret
 
-    ; Exit the program
-    mov rax, 60                 ; syscall: exit
-    xor rdi, rdi                ; status: 0
-    syscall
-
+format: db 'Hello, Holberton\n',0
