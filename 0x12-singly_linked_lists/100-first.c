f1228019f94c4dfd6a49a605f513fb44d159107b
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-21 19:12:57 -0700

    h

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+void print(void) __attribute__((constructor));
+/**
+ * print - it is a func
+ * Return: return
+ */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
