f9163d01f13fe4322a2395bfdd193fe54fe8c926
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-24 01:51:28 -0700

    g

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,24 +1,39 @@
 #include "main.h"
 /**
+ * is_valid_char - it is a func
+ * @c: it is a char
+ * Return: alpha
+ */
+int is_valid_char(char c)
+{
+return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+/**
  * is_palindrome - it is a func
  * @s: it is a var
  * Return: return
  */
 int is_palindrome(char *s)
 {
-	int length = strlen(s);
-	int start = 0;
-	int end = length - 1;
+	int le = 0;
+	int ri = strlen(s) - 1;
 
-	while (start < end)
+	while (le < ri)
 	{
-	if (s[start] != s[end])
-	{
-		return (0);
+		while (!is_valid_char(s[le]) && le < ri)
+		{
+			le++;
+		}
+		while (!is_valid_char(s[ri]) && le < ri)
+		{
+			ri--;
+		}
+		if (s[le] != s[ri])
+		{
+			return (0);
+		}
+		le++;
+		ri--;
 	}
-	start++;
-	end--;
-	}
-
 	return (1);
 }
