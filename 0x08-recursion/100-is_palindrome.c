729de4b2e571d20e84c5c7091fb6f19ca7edf6c8
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-24 02:13:33 -0700

    g

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,39 +1,49 @@
 #include "main.h"
 /**
- * is_valid_char - it is a func
- * @c: it is a char
+ * is_char - it is a func
+ * @c: it is char
  * Return: alpha
  */
-int is_valid_char(char c)
+int is_char(char c)
 {
-return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+	return ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'));
+}
+/**
+ * check_palindrome - it is a func to check
+ * @s: it is pointer
+ * @left: it is left
+ * @right: it is right
+ * Return: return
+ */
+int check_palindrome(char *s, int left, int right)
+{
+	if (left >= right)
+	{
+		return (1);
+	}
+	while (!is_char(s[left]) && left < right)
+	{
+		left++;
+	}
+	while (!is_char(s[right]) && left < right)
+	{
+		right--;
+	}
+
+	if (s[left] != s[right])
+	{
+		return (0);
+	}
+		return (check_palindrome(s, left + 1, right - 1));
 }
 /**
  * is_palindrome - it is a func
- * @s: it is a var
+ * @s: it is
  * Return: return
  */
 int is_palindrome(char *s)
 {
-	int le = 0;
-	int ri = strlen(s) - 1;
+	int length = strlen(s);
 
-	while (le < ri)
-	{
-		while (!is_valid_char(s[le]) && le < ri)
-		{
-			le++;
-		}
-		while (!is_valid_char(s[ri]) && le < ri)
-		{
-			ri--;
-		}
-		if (s[le] != s[ri])
-		{
-			return (0);
-		}
-		le++;
-		ri--;
-	}
-	return (1);
+	return (check_palindrome(s, 0, length - 1));
 }
