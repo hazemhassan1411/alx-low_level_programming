d1feb901eb464beb300e4bdd68f14ec86d2f437c
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-26 15:28:56 -0700

    j

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+/**
+ * get_nodeint_at_index - it is func
+ * @head: it is a pointer
+ * @index:it is integr
+ * Return: return
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *node;
+	int j = 0;
+
+	for (node = head; node && j < index; node = node->next, j++)
+		;
+	return (node);
+}
