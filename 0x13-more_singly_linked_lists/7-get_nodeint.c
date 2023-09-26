43abcfce1830811ca00a1efdb1ac96a9a350f997
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-09-26 15:30:04 -0700

    j

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -8,7 +8,7 @@
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	listint_t *node;
-	int j = 0;
+	unsigned int j = 0;
 
 	for (node = head; node && j < index; node = node->next, j++)
 		;
