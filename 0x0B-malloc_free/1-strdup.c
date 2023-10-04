badf47b736c9949421d77e2638592c8579bd594e
root <hazemhassan1411>
2023-10-04 05:48:22 -0700

    g

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strdup - it is a func
+ * @str: it is integr
+ * Return: the array
+*/
+char *_strdup(char *str);
+{
+	int i, s;
+	char *m;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (s = 0 ; str[s] != '\0'; s++)
+		;
+
+m = malloc(s * sizeof(*str) + 1);
+
+	if (m == 0)
+		return (NULL);
+
+	else
+	{
+	for (i = 0; i < s; i++)
+		m[i] = str[i];
+	}
+	return (m);
+}
