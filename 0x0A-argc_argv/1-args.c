48677d4ebecfd3347c4ede4f9de307a72ed6d1fc
root <hazemhassan1411>
2023-10-08 01:29:39 -0700

    f

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - it is func
+ * @argc: it is i
+ * @argv: it is char
+ * Return: return
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
