b05e1fe62b2e7303c74813fa71e7a149d56f108b
root <hazemhassan1411>
2023-10-08 01:19:26 -0700

    g

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,11 @@
 /**
  * main - it is a func
  * @argv: it is string
+ * @argv: it is i 
  * Return: return the name
  */
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
