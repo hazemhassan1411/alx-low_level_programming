7504c8827453bd4a814a58d3dbaa6b721960e2d9
root <hazemhassan1411>
2023-10-11 12:42:11 -0700

    f

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_name - it is a func
+ * @name: it is a name
+ * @f: it is func
+ * Return: return
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f && name)
+		f(name);
+}
