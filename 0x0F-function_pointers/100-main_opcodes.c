9cd23da26ca685d59a8eeb12171b61d536c24ed0
hazemhassan1411 <138767233+hazemhassan1411@users.noreply.github.com>
2023-10-17 16:29:56 +0300

    Create 100-main_opcodes.c
    o

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - it is main
+ * @argc: it is argc
+ * @argv: it is argv
+ * Return:return 0
+ */
+
+int main(int argc, char **argv)
+{
+        char *f = (char *)main;
+        int h;
+
+        if (argc != 2)
+                printf("Error\n"), exit(1);
+        h = atoi(argv[1]);
+        if (h < 0)
+                printf("Error\n"), exit(2);
+        while (h--)
+                printf("%02hhx%s", *f++, h ? " " : "\n");
+        return (0);
+}
