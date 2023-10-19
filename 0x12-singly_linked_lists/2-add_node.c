9156af128ce632d9d8d82c1f666230ea86032fb0
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-10-19 05:33:31 -0700

    f

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,6 +11,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	list_t *new_node = malloc(sizeof(list_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
@@ -22,7 +23,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->len = strlen(str);
-	new_node->next  *head;
+	new_node->next  * head;
 	*head = new_node;
 
 	return (new_node);
