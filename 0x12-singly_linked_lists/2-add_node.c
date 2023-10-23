5e524f138a37ae866632c8d53e7014c8e3497167
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-10-23 03:51:59 -0700

    f

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,29 +1,32 @@
 #include "lists.h"
+
 /**
- * add_node - Adds a new node at the beginning of a list_t list.
- * @head: A pointer to a pointer to the head of the list.
- * @str: The string to be duplicated and stored in the new node.
- * Return: The address of the new element, or NULL if it failed.
+ * add_node-a function adds a new node at the beginning of a list_t list.
+ * @head: pointer_____
+ *@str:string_______
+ * Return: returns kok ______
  */
+
 list_t *add_node(list_t **head, const char *str)
-{
-if (!head || !str)
-return (NULL);
 
-list_t *new_node = malloc(sizeof(list_t));
-if (!new_node)
-return (NULL);
-
-new_node->str = strdup(str);
-if (!new_node->str)
 {
-free(new_node);
+list_t *kok = malloc(sizeof(list_t));
+
+if (!head || !kok)
+{
 return (NULL);
 }
-
-new_node->len = strlen(str);
-new_node->next = *head;
-*head = new_node;
-
-return (new_node);
+if (str)
+{
+kok->str = strdup(str);
+if (!kok->str)
+{
+free(kok);
+return (NULL);
+}
+kok->len = strlen(kok->str);
+}
+kok->next = *head;
+*head = kok;
+return (kok);
 }
