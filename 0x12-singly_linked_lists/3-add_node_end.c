c725b9d11bf2e5a2638eeb666f3a465dda5b9756
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-10-23 03:54:56 -0700

    f

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ * add_node_end -a function adds a new node at the end of a list_t list.___
+ * @head: pointer_____
+ * @str: string _______
+ * Return: no returns  ______
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *kok = malloc(sizeof(list_t));
+	list_t *non;
+
+	if (!head || !kok)
+		return (NULL);
+
+	kok->str = strdup(str);
+	if (!kok->str)
+	{
+		free(kok);
+		return (NULL);
+	}
+
+	kok->len = strlen(kok->str);
+	kok->next = NULL;
+
+	if (*head == NULL)
+	{
+	*head = kok;
+	}
+	else
+	{
+	non = *head;
+	while (non->next != NULL)
+		non = non->next;
+		non->next = kok;
+	}
+	return (kok);
+}
