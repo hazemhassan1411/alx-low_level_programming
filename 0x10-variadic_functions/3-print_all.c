21bdf4423c049830046e2a3559afb5dc64946c3d
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-10-23 03:45:03 -0700

    f

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,47 +1,51 @@
 #include "variadic_functions.h"
+
 /**
- * print_all - it is a func
- * @format: it is a format
- * Return: return
+ * print_all -  a function that prints anything____
+ * @format: format of data type_____
+ * Return: no returns  ______
  */
+
 void print_all(const char * const format, ...)
+
 {
-	va_list args;
-        char *separator = "";
-        const char *ptr = format;
-
-	va_start(args, format);
-
-	while (*ptr != '\0')
-	{
-	if (*ptr == 'c')
-	{
-		printf("%s%c", separator, va_arg(args, int));
-	}
-	else if (*ptr == 'i')
-	{
-		printf("%s%d", separator, va_arg(args, int));
-	}
-	else if (*ptr == 'f')
-	{
-		printf("%s%f", separator, va_arg(args, double));
-	}
-	else if (*ptr == 's')
-	{
-	char *str = va_arg(args, char*);
-
-	if (str == NULL)
-	{
-		printf("%s(nil)", separator);
-	}
-	else
-	{
-		printf("%s%s", separator, str);
-	}
-	}
-	separator = ", ";
-	ptr++;
-	}
-	va_end(args);
-	printf("\n");
+int frist;
+int second;
+char *string;
+va_list lol;
+va_start(lol, format);
+frist = 0;
+while (format && format[frist])
+{
+switch (format[frist])
+{
+case 'i':
+printf("%d", va_arg(lol, int));
+second = 0;
+break;
+case 'f':
+printf("%f", va_arg(lol, double));
+second = 0;
+break;
+case 'c':
+printf("%c", va_arg(lol, int));
+second = 0;
+break;
+case 's':
+string = va_arg(lol, char *);
+if (string == NULL)
+string = "(nil)";
+printf("%s", string);
+second = 0;
+break;
+default:
+second = 1;
+break;
+}
+if (format[frist + 1] != '\0' && second == 0)
+printf(", ");
+frist++;
+}
+printf("\n");
+va_end(lol);
 }
