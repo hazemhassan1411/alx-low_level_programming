59d09986aed392a36d98c11de9bbb568fcb571cd
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-11-05 14:01:42 +0200

    done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,10 +17,10 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 		fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
 	}
-	if (fd == -1)
+	else if (fd == -1)
 		return (-1);
 
-	if (text_content != NULL)
+	else if (text_content != NULL)
 	{
 		bytes_written = write(fd, text_content, strlen(text_content));
 
