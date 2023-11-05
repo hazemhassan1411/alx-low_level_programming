fb712da3d326dd12f69c20410eaeef29bf0e2dbd
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-11-05 14:11:06 +0200

    d

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Create a file with given text content.
+ * @filename: Name of the file to create.
+ * @text_content: Text content to write to the file.
+ * Return: 1 on success, -1 on failure.
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd;
+	ssize_t bytes_written;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		bytes_written = write(fd, text_content, strlen(text_content));
+		close(fd);
+
+		if (bytes_written == -1)
+			return (-1);
+	}
+	else
+	{
+		close(fd);
+	}
+
+	return (1);
+}
