96b6d766981eff2cfd867801fb5896d7fe39ec73
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-11-05 15:04:53 +0200

    d

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
 #include <fcntl.h>
@@ -7,42 +6,42 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 
-typedef struct {
-	unsigned char e_ident[16];
-	uint16_t e_type;
-	uint16_t e_machine;
-	uint32_t e_version;
-	uint64_t e_entry;
-	uint64_t e_phoff;
-	uint64_t e_shoff;
-	uint32_t e_flags;
-	uint16_t e_ehsize;
-	uint16_t e_phentsize;
-	uint16_t e_phnum;
-	uint16_t e_shentsize;
-	uint16_t e_shnum;
-	uint16_t e_shstrndx;
-} Elf64_Ehdr;
 
+
+/**
+ * print_elf_header_info - Print information from the ELF header
+ * @header: Pointer to the ELF header
+ */
 void print_elf_header_info(const Elf64_Ehdr *header)
 {
 	int i;
-	
+
 	printf("ELF Header:\n");
 	printf("  Magic:   ");
-	for (i = 0; i < 16; i++) {
+	for (i = 0; i < 16; i++)
+	{
 		printf("%02x ", header->e_ident[i]);
 	}
 	printf("\n");
-	printf("  Class:                             ELF%d\n", header->e_ident[4] == 1 ? 32 : 64);
-	printf("  Data:                              2's complement, %s endian\n", header->e_ident[5] == 1 ? "little" : "big");
-	printf("  Version:                           %u (current)\n", header->e_version);
-	printf("  OS/ABI:                            %s\n", header->e_ident[7] == 0 ? "UNIX - System V" : "unknown");
+	printf("  Class:                             ELF%d\n",
+		   header->e_ident[4] == 1 ? 32 : 64);
+	printf("  Data:                              2's complement, %s endian\n",
+		   header->e_ident[5] == 1 ? "little" : "big");
+	printf("  Version:                           %u\n", header->e_version);
+	printf("  OS/ABI:                            %s\n",
+		   header->e_ident[7] == 0 ? "UNIX - System V" : "unknown");
 	printf("  ABI Version:                       %u\n", header->e_ident[8]);
-	printf("  Type:                              %s (Executable file)\n", header->e_type == 2 ? "EXEC" : "DYN");
+	printf("  Type:                              %s (Executable file)\n",
+		   header->e_type == 2 ? "EXEC" : "DYN");
 	printf("  Entry point address:               0x%lx\n", header->e_entry);
 }
 
+/**
+ * main - Entry point of the program
+ * @argc: Argument count
+ * @argv: Argument vector
+ * Return: 0 on success, 98 on error
+ */
 int main(int argc, char *argv[])
 {
 	int fd;
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -9,6 +9,43 @@
 
 #define BUF_SIZE 1024
 
+/**
+ * struct Elf64_Ehdr - ELF header struct
+ * @e_ident:   ELF identification bytes
+ * @e_type:    Type of ELF file
+ * @e_machine: Target architecture
+ * @e_version: ELF version
+ * @e_entry:   Entry point address
+ * @e_phoff:   Program header table offset
+ * @e_shoff:   Section header table offset
+ * @e_flags:   Processor-specific flags
+ * @e_ehsize:  ELF header size
+ * @e_phentsize: Program header entry size
+ * @e_phnum:   Number of program header entries
+ * @e_shentsize: Section header entry size
+ * @e_shnum:   Number of section header entries
+ * @e_shstrndx: Section name string table index
+ */
+
+
+typedef struct Elf64_Ehdr
+{
+	unsigned char e_ident[16];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint64_t e_entry;
+	uint64_t e_phoff;
+	uint64_t e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
+} Elf64_Ehdr;
+
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
