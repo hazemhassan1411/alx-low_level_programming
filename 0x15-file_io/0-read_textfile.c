224ccf5a062ec082cd409587d0070244324aea20
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-11-05 13:48:16 +0200

    done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -12,7 +12,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	int filed;
 	ssize_t byte;
 
-	char b{BUF_SIZE * 8};
+	char buf[BUF_SIZE * 8];
 
 	if (!filename || !letters)
 	return (0);
@@ -21,8 +21,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (filed == -1)
 	return (0);
 
-	byte = read(filed, &b[0], letters);
-	byte = write(STDOUT_FILENO, &b[0], byte);
+	byte = read(filed, &buf[0], letters);
+	byte = write(STDOUT_FILENO, &buf[0], byte);
 	close(filed);
 	return (byte);
 }
