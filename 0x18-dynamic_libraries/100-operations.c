eb602eebfc47f68902cbbf53884466441cc43689
hazemhassan1411 <hazemhassan1411@gmail.com>
2023-12-11 15:40:23 +0200

    rr

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,26 +1,52 @@
 #include <stdio.h>
-
+/**
+ * add - add
+ * @a: first integer
+ * @b: second integer
+ * Return: add
+ */
 int add(int a, int b)
 {
-    return (a + b);
+	return (a + b);
 }
+
+/**
+ * sub - sub
+ * @a: first integer
+ * @b: second integer
+ * Return: sub
+ */
 
 int sub(int a, int b)
 {
-    return (a - b);
+	return (a - b);
 }
+
+/**
+ * mul - mul
+ * @a: first integer
+ * @b: second integer
+ * Return: mul
+ */
 
 int mul(int a, int b)
 {
-    return (a * b);
+	return (a * b);
 }
+
+/**
+ * div - div
+ * @a: first integer
+ * @b: second integer
+ * Return: div
+ */
 
 int div(int a, int b)
 {
-    if (b == 0)
-    {
-        printf("Error: Division by zero\n");
-        return (0);
-    }
-    return (a % b);
+	if (b == 0)
+	{
+		printf("Error: Division by zero\n");
+		return (0);
+	}
+	return (a % b);
 }
